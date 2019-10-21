b1dbaeb080ea2f177a88e52617e5071009911607
Dennis Nesanoff <nndennisnn@gmail.com>
2019-10-21 08:03:34 +0300

    Setup UI

---
+++ NavigationApp/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  NavigationApp
+//
+//  Created by Dennis Nesanoff on 21.10.2019.
+//  Copyright © 2019 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
